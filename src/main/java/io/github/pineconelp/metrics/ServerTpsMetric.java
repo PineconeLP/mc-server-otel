536538a8071f0b34c30b5dd01557c530aac0b835
PineconeLP <pineconelp4@gmail.com>
2026-05-14 21:37:52 -0400

    Report all TPS windows

--- src/main/java/io/github/pineconelp/metrics/ServerTpsMetric.java
+++ src/main/java/io/github/pineconelp/metrics/ServerTpsMetric.java
@@ -1,5 +1,7 @@
 package io.github.pineconelp.metrics;
 
+import io.opentelemetry.api.common.AttributeKey;
+import io.opentelemetry.api.common.Attributes;
 import io.opentelemetry.api.metrics.Meter;
 import io.opentelemetry.api.metrics.ObservableDoubleGauge;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -12,12 +14,22 @@ public class ServerTpsMetric implements MinecraftMetric {
     this.plugin = plugin;
   }
 
+  private static final AttributeKey<String> WINDOW_KEY = AttributeKey.stringKey("window");
+  private static final Attributes WINDOW_1M = Attributes.of(WINDOW_KEY, "1m");
+  private static final Attributes WINDOW_5M = Attributes.of(WINDOW_KEY, "5m");
+  private static final Attributes WINDOW_15M = Attributes.of(WINDOW_KEY, "15m");
+
   @Override
   public void register(Meter meter) {
     gauge = meter.gaugeBuilder("minecraft.server.tps")
         .setDescription("Server ticks per second")
         .setUnit("{tps}")
-        .buildWithCallback(measurement -> measurement.record(plugin.getServer().getTPS()[0]));
+        .buildWithCallback(measurement -> {
+          double[] tps = plugin.getServer().getTPS();
+          measurement.record(tps[0], WINDOW_1M);
+          measurement.record(tps[1], WINDOW_5M);
+          measurement.record(tps[2], WINDOW_15M);
+        });
   }
 
   @Override
